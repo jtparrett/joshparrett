6b4964f701b4cf20a4c703af3ad855f7942d729a
Josh Parrett <jtparrett@me.com>
2024-01-21 09:43:46 +0000

    project setup

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,104 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Box, Container, Flex, styled } from "~/styled-system/jsx";
+import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Josh Parrett - CV" },
+    { name: "description", content: "Welcome to my online curriculum vitae." },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
+    <Container py={12} maxW={900}>
+      <Flex gap={2}>
+        <Box
+          flex={1}
+          pos="relative"
+          zIndex={1}
+          _after={{
+            content: '""',
+            h: 1,
+            bgColor: "brand",
+            w: 280,
+            pos: "absolute",
+            bottom: 18,
+            zIndex: -1,
+            left: -4,
+          }}
+        >
+          <styled.h1
+            mb={2}
+            fontWeight="black"
+            fontSize="6xl"
+            textTransform="uppercase"
+            lineHeight={0.8}
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+            <styled.span
+              fontWeight="medium"
+              fontSize="2xl"
+              textTransform="capitalize"
+            >
+              Josh Parrett's
+            </styled.span>
+            <br />
+            Curriculum
+            <br />
+            Vitae
+          </styled.h1>
+        </Box>
+
+        <styled.a
+          fontSize="2xl"
+          href="https://www.linkedin.com/in/jtparrett/"
+          target="_blank"
+        >
+          <IoLogoLinkedin />
+          <styled.span srOnly>LinkedIn</styled.span>
+        </styled.a>
+        <styled.a
+          fontSize="2xl"
+          href="https://github.com/jtparrett"
+          target="_blank"
+        >
+          <IoLogoGithub />
+          <styled.span srOnly>GitHub</styled.span>
+        </styled.a>
+      </Flex>
+
+      <Flex
+        gap={4}
+        alignItems="flex-start"
+        flexDir={{ base: "column-reverse", md: "row" }}
+      >
+        <styled.p color="gray.400" mb={4} flex={1} textWrap="balance">
+          With over 11 years of professional design & development work, pushing
+          cutting-edge technologies and working across numerous startups owning
+          product development. I am a passionate engineer, designer and leader,
+          dedicating my free time to all things{" "}
+          <styled.span color="brand" fontWeight="medium">
+            Typescript, NodeJS, React and Serverless;
+          </styled.span>{" "}
+          whilst working on a number of Node/React personal, commercial and
+          professional projects at a large scale.
+          <br />
+          <br /> I am incredibly keen on following and setting standards whilst
+          maintaining the bigger picture and staying on a tight target. Striving
+          for organised programming, clean code and innovative approaches to
+          tasks. I'm a dedicated team player with an amazing ability and
+          ambition to solve problems and develop performant features with
+          exceptional attention to detail.
+        </styled.p>
+
+        <Box rounded="lg" overflow="hidden" w={240}>
+          <styled.img src="/josh-parrett.jpg" w="full" display="block" />
+        </Box>
+      </Flex>
+
+      <styled.h2 fontWeight="black" fontSize="4xl" textTransform="uppercase">
+        Roles
+      </styled.h2>
+    </Container>
   );
 }
---
+++ panda.config.ts
@@ -0,0 +1,82 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // The extension for the emitted JavaScript files
+  outExtension: "js",
+  // Where to look for your css declarations
+  include: [
+    "./app/routes/**/*.{ts,tsx,js,jsx}",
+    "./app/components/**/*.{ts,tsx,js,jsx}",
+  ],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {
+      tokens: {
+        fonts: {
+          body: {
+            value: '"ClashDisplay-Variable", sans-serif',
+          },
+        },
+        colors: {
+          brand: {
+            value: "#e61a4c",
+          },
+          "gray.50": {
+            value: "#fafafa",
+          },
+          "gray.100": {
+            value: "#f4f4f5",
+          },
+          "gray.200": {
+            value: "#e4e4e7",
+          },
+          "gray.300": {
+            value: "#d4d4d8",
+          },
+          "gray.400": {
+            value: "#a1a1aa",
+          },
+          "gray.500": {
+            value: "#71717a",
+          },
+          "gray.600": {
+            value: "#52525b",
+          },
+          "gray.700": {
+            value: "#3f3f46",
+          },
+          "gray.800": {
+            value: "#27272a",
+          },
+          "gray.900": {
+            value: "#18181b",
+          },
+          "gray.950": {
+            value: "#09090b",
+          },
+        },
+      },
+    },
+  },
+
+  globalCss: {
+    html: {
+      backgroundColor: "gray.950",
+      color: "white",
+      fontFamily: "body",
+      lineHeight: 1.2,
+    },
+  },
+
+  // The output directory for your css system
+  outdir: "./app/styled-system",
+
+  jsxFramework: "react",
+});
